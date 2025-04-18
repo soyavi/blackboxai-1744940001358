8681885e72d3ffe475f22964f1f19a496ae80f9c
soyavi <admin@loveavi.com>
2025-04-17 19:33:51 -0600

    Add src/config/Settings.js 1744940030658

---
+++ src/config/Settings.js
@@ -0,0 +1,10 @@
+/*
+API_URL: a string that represents the URL of the ChatGenius API
+DEEP_LINK_SCHEME: a string that represents the deep link scheme used by ChatGenius
+DEFAULT_DARK: a boolean value that indicates whether the app should use dark mode by default
+ */
+module.exports = {
+  API_URL: 'https://api.soyavi.com/api',
+  DEEP_LINK_SCHEME: 'chatgenius',
+  DEFAULT_DARK: false,
+};
